2fa9aa4a03ccc41a2101398e1148b8b89ec05f80
Raphael S. <salajaraphael@gmail.com>
2024-09-16 07:44:57 +0100

    chore: styling update

--- app/blog/components/posts.tsx
+++ app/blog/components/posts.tsx
@@ -11,26 +11,30 @@ export function Posts() {
     return 1;
   });
 
-  // short month/year all
   const formatter = new Intl.DateTimeFormat("en", {
+    day: "2-digit",
     month: "2-digit",
-    year: "numeric",
+    year: "2-digit",
   });
 
   return (
-    <div className="mt-8 flex flex-col">
+    <div className="mt-6 flex flex-col">
+      <h1 className="pb-2">
+        Thoughts {posts.length > 0 && `(${posts.length})`}
+      </h1>
       {posts.map((post, index) => (
         <Link key={post.slug} href={`/blog/${post.slug}`}>
           <div
             className={clsx({
-              "mb-2 flex w-full gap-4 pb-2 transition-all": true,
+              " flex w-full justify-between py-2 transition-all": true,
               "border-b border-b-gray-4": index !== posts.length - 1,
+              "border-t border-t-gray-4": index === 0,
             })}
           >
+            <p>{post.title}</p>
             <p className="text-gray-8">
               {formatter.format(new Date(post.time.created))}
             </p>
-            <p>{post.title}</p>
           </div>
         </Link>
       ))}
