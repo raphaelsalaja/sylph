b0063856ba8238b12aed4fce69a9a46ea2de1471
Raphael S. <salajaraphael@gmail.com>
2024-09-27 06:44:15 +0100

    revert: next-config

--- next.config.mjs
+++ next.config.mjs
@@ -2,23 +2,8 @@ import nextMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  cleanDistDir: true,
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   reactStrictMode: true,
-  poweredByHeader: false,
-  pageExtensions: ["md", "mdx", "tsx", "ts", "jsx", "js"],
-  env: {
-    NEXT_TELEMETRY_DISABLED: "1",
-  },
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
-      },
-    },
-  },
 };
 
 const withMDX = nextMDX({
