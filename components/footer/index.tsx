f9ee195a86956a9a3800b35dc67c6cf4f7f612eb
Raphael S. <salajaraphael@gmail.com>
2024-09-28 12:51:12 +0100

    fix: missing import + new /public

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,11 +1,11 @@
-import { Link as LabeledLink } from "@/components/link";
+import Link from "@/components/link";
 import * as Theme from "@/components/theme";
 
 const Footer = () => {
   return (
     <div className="flex w-full items-center justify-between border-border border-t pt-2">
       <div className="px-[2px] text-muted text-small tracking-[0.01px]">
-        Built with <LabeledLink href="https://nextjs.org/" text="Next.js" underline />
+        Built with <Link href="https://nextjs.org/" text="Next.js" underline />
       </div>
       <div className="text-muted text-small tracking-[0.01px]">
         <Theme.Switch />
