27cce7c8aee6dde989b70c0a3e8cd8bfcaa8caa0
Raphael S. <salajaraphael@gmail.com>
2024-09-29 21:26:57 +0100

    feat: slimmer layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body>
         <ViewTransitions>
           <Theme.Provider>
-            <main className="mx-auto max-w-screen-md overflow-x-hidden px-6 py-24 md:overflow-x-visible ">
+            <main className="mx-auto max-w-screen-sm overflow-x-hidden px-6 py-24 md:overflow-x-visible ">
               <article className="article">{children}</article>
             </main>
           </Theme.Provider>
