d3f3491d6ac2050f7d5e0b21b1bd22a71f48ffbe
Raphael S. <52125687+raphaelsalaja@users.noreply.github.com>
2024-10-16 21:10:40 +0100

    feat: new readme + better theme switcher

--- components/theme/index.tsx
+++ components/theme/index.tsx
@@ -19,34 +19,42 @@ const Switch = () => {
   if (!mounted) return null;
 
   const buttons = [
+    {
+      label: "system",
+      icon: <Monitor width={13} />,
+      active: theme === "system",
+    },
     { label: "dark", icon: <Moon width={13} />, active: theme === "dark" },
     { label: "light", icon: <Sun width={13} />, active: theme === "light" },
   ];
 
-  const common = "flex items-center justify-center w-6 h-6 rounded transition-all hover:opacity-50";
-
   return (
-    <div className="flex gap-2">
-      <span className="flex w-fit items-center overflow-hidden rounded-md border border-border bg-gray-2">
-        <button type="button" onClick={() => setTheme("system")} className={cn(common, theme === "system" ? "bg-gray-4 text-foreground" : "")}>
-          <Monitor width={13} />
+    <span className="flex w-fit items-center gap-0.5 overflow-hidden rounded-[6px] p-[2px] bg-gray-2">
+      {buttons.map(({ label, icon, active }) => (
+        <button
+          type="button"
+          key={label}
+          onClick={() => setTheme(label)}
+          className={cn(
+            "flex items-center justify-center w-6 h-6 rounded-[4px]  ransition-all hover:opacity-50",
+            active ? "bg-gray-4 shadow text-foreground" : "",
+          )}
+        >
+          {icon}
         </button>
-      </span>
-
-      <span className="flex w-fit items-center gap-0.5 overflow-hidden rounded-md border border-border bg-gray-2">
-        {buttons.map(({ label, icon, active }) => (
-          <button type="button" key={label} onClick={() => setTheme(label)} className={cn(common, active ? "bg-gray-4 text-foreground" : "")}>
-            {icon}
-          </button>
-        ))}
-      </span>
-    </div>
+      ))}
+    </span>
   );
 };
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ThemeProvider enableSystem={true} attribute="class" storageKey="theme" defaultTheme="system">
+    <ThemeProvider
+      enableSystem={true}
+      attribute="class"
+      storageKey="theme"
+      defaultTheme="system"
+    >
       {children}
     </ThemeProvider>
   );
