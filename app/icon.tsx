4b650e503ea8a46f73c1568bf2cf19115eb51523
Raphael S. <52125687+raphaelsalaja@users.noreply.github.com>
2024-10-17 17:16:16 +0100

    feat: simple favicon

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,3 +1,4 @@
+import { gray } from "@radix-ui/colors";
 import { ImageResponse } from "next/og";
 
 export const size = {
@@ -8,17 +9,32 @@ export const contentType = "image/png";
 
 export default function Icon() {
   return new ImageResponse(
-    <div
-      style={{
-        background: "#BDEE63",
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: "50%",
-      }}
-    />,
+    (
+      <div
+        style={{
+          background: gray.gray4,
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 100,
+          padding: 2,
+        }}
+      >
+        <div
+          style={{
+            background: gray.gray12,
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 100,
+          }}
+        />
+      </div>
+    ),
     {
       ...size,
     },
