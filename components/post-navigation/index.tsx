7d366619cdabf46082768024eb399f7b75972283
Raphael S. <52125687+raphaelsalaja@users.noreply.github.com>
2024-10-17 21:06:13 +0100

    fix: use next transitions for the post navigation

--- components/post-navigation/index.tsx
+++ components/post-navigation/index.tsx
@@ -2,6 +2,7 @@
 
 import type { Post } from "@/types/post/index";
 
+import { Link } from "next-view-transitions";
 import { usePathname } from "next/navigation";
 
 interface PostNavigationProps {
@@ -10,12 +11,15 @@ interface PostNavigationProps {
 
 function PostNavigation({ posts }: PostNavigationProps) {
   posts.sort((a, b) => {
-    return new Date(b.time.created).getTime() - new Date(a.time.created).getTime();
+    return (
+      new Date(b.time.created).getTime() - new Date(a.time.created).getTime()
+    );
   });
 
   const currentSlug = usePathname().split("/").pop();
   const currentIndex = posts.findIndex((post) => post.slug === currentSlug);
-  const previous = currentIndex < posts.length - 1 ? posts[currentIndex + 1] : null;
+  const previous =
+    currentIndex < posts.length - 1 ? posts[currentIndex + 1] : null;
   const next = currentIndex > 0 ? posts[currentIndex - 1] : null;
 
   if (!previous && !next) {
@@ -25,16 +29,22 @@ function PostNavigation({ posts }: PostNavigationProps) {
   return (
     <div className="mt-16 flex w-full justify-between border-border border-t pt-8">
       {previous && (
-        <a href={`${previous.slug}`} className="flex w-full flex-col gap-1 text-left">
+        <Link
+          href={`${previous.slug}`}
+          className="flex w-full flex-col gap-1 text-left"
+        >
           <span className="text-muted">Previous</span>
           <span>{previous.title}</span>
-        </a>
+        </Link>
       )}
       {next && (
-        <a href={`${next.slug}`} className="flex w-full flex-col gap-1 text-right">
+        <Link
+          href={`${next.slug}`}
+          className="flex w-full flex-col gap-1 text-right"
+        >
           <span className="text-muted">Next</span>
           <span>{next.title}</span>
-        </a>
+        </Link>
       )}
     </div>
   );
