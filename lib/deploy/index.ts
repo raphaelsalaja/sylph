635a73b55de719578d6e1a209962885007d97fa7
Raphael S. <52125687+raphaelsalaja@users.noreply.github.com>
2024-10-18 15:10:17 +0100

    refactor: leave in portfolio image, use vercel deploy button styling

--- lib/deploy/index.ts
+++ lib/deploy/index.ts
@@ -5,5 +5,9 @@ const REDIRECT_URL = "https://twitter.com/raphaelsalaja";
 const DEMO_TITLE = "next-slyph-portfolio";
 const DEMO_DESCRIPTION = "A minimal blog built with Next.js.";
 const DEMO_URL = "https://next-sylph-portfolio.vercel.app";
+const DEMO_IMAGE =
+  "https://raw.githubusercontent.com/raphaelsalaja/sylph/refs/heads/main/.github/assets/readme.png";
 
-export const DeployLink = `https://vercel.com/new/clone?repository-url=${encodeURIComponent(REPOSITORY_URL)}&env=NEXT_PUBLIC_SITE_URL&project-name=${PROJECT_NAME}&repository-name=${REPOSITORY_NAME}&redirect-url=${encodeURIComponent(REDIRECT_URL)}&demo-title=${DEMO_TITLE}&demo-description=${encodeURIComponent(DEMO_DESCRIPTION)}&demo-url=${encodeURIComponent(DEMO_URL)}`;
+const DeployLink = `https://vercel.com/new/clone?repository-url=${encodeURIComponent(REPOSITORY_URL)}&env=NEXT_PUBLIC_SITE_URL&project-name=${PROJECT_NAME}&repository-name=${REPOSITORY_NAME}&redirect-url=${encodeURIComponent(REDIRECT_URL)}&demo-title=${DEMO_TITLE}&demo-description=${encodeURIComponent(DEMO_DESCRIPTION)}&demo-url=${encodeURIComponent(DEMO_URL)}&demo-image=${encodeURIComponent(DEMO_IMAGE)}`;
+
+export { DeployLink };
